0e42316214107e7d67de198b41ccd9a18173d821
Montique Stevens <mstevens528@gmail.com>
2021-02-23 18:44:27 -0500

    Create Forecast.js
    added getForecast function

---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,18 @@
+import React from 'react' ;
+
+/* adding OpenWeather API to file */
+
+const Forecast = () => {
+
+    function getForecast() {
+
+    // add weather data fetch  function
+    }
+
+    return (
+        // JSX goes here
+
+    )
+}
+
+export default Forecast;
