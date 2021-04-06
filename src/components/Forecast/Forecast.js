21f7a269b297f27f3b2ceffe4f5fa7a5e69337fd
Montique Stevens <mstevens528@gmail.com>
2021-04-06 12:12:29 -0400

    Update Forecast.js
    added fetch function for weather data for New York

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -6,7 +6,22 @@ const Forecast = () => {
 
     function getForecast() {
 
-    // add weather data fetch function here
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=New%20York&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "f7e3448277msha25ddce647d5337p103cf8jsn14d27353503a",
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+   
+   // add weather data fetch function here
+   
     }
 
     return (
