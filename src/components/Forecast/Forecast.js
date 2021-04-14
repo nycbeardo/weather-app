c4542faa8274a8823bffa0ca50540b58431460b4
Montique Stevens <mstevens528@gmail.com>
2021-04-14 16:02:23 -0400

    Update Forecast.js

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -24,36 +24,39 @@ const uriEncodedCity = encodeURIComponent(city);
 
 
 
-    function getForecast(e) {
-        e.preventDefault();
-
-        if (city.length === 0) {
-            return setError(true);
-        }
-
-        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "f7e3448277msha25ddce647d5337p103cf8jsn14d27353503a",
-                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-            }
-        })
-        .then(response => response.json())
-        .then(response => {setResponseObj(response) ;
-        
-        
-        
-
-       
-
-        });
-        
-        
-      // add weather data for fetch function here above this line
-
-   
-   
+function getForecast(e) {
+    e.preventDefault();
+    if (city.length === 0) {
+        return setError(true);
     }
+    // Clear state in preparation for new data
+    setError(false);
+    setResponseObj({});
+   
+    setLoading(true);
+   
+    let uriEncodedCity = encodeURIComponent(city);
+ fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key": "apikey"
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        if (response.cod !== 200) {
+            throw new Error()
+        }
+        setResponseObj(response);
+        setLoading(false);
+    })
+    .catch(err => {
+        setError(true);
+        setLoading(false);
+        console.log(err.message);
+    });
+ }
 
 
 
