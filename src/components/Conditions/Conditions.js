0ab33c13ecca1720978653850ed53a836f9bbaff
Montique Stevens <mstevens528@gmail.com>
2021-04-16 10:35:33 -0400

    misc

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -2,19 +2,23 @@ import React from 'react';
 import classes from './Conditions.module.css'
 
 const conditions = (props) => {
-   return (
-       <div className={classes.Wrapper}>
-       {props.error && <small>Please enter a valid city.</small>}
-       {props.loading && <div>Loading...</div>}
-           {props.responseObj.cod === 200 ?
-               <div>
-                   <p><strong>{props.responseObj.name}</strong></p>
-                   <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
-               </div>
-           : null
-           }
-       </div>
-   )
+    return (
+        <div className={classes.Wrapper}>
+
+            {props.error && <small className={classes.Small}>Please enter a valid city.</small>}
+
+            {props.loading && <div className={classes.Loader} />}
+
+
+            {props.responseObj.cod === 200 ?
+                <div>
+                    <p><strong>{props.responseObj.name}</strong></p>
+                    <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+                </div>
+            : null
+            }
+        </div>
+    )
 }
 
 export default conditions;
