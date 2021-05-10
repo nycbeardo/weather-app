3f7e6fb66a4db211d0987a88c19d43adc03c479f
Montique Stevens <mstevens528@gmail.com>
2021-05-10 11:34:05 -0400

    Misc fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Logo from './components/Logo/Logo';
 function App() {
   return (
     <div className="App  flex justify-center items-center bg-gray-100">
-    <div className="bg-green-800  shadow-2xl rounded-lg border-solid border-4 border-green-800 ">
+    <div className="bg-green-700  shadow-2xl rounded-lg border-solid border-4 border-green-800 ">
       <header className="App-header">
         <Logo className=" " />
         <h1 className="font-bold text-white flex justify-center items-center ">React Weather App</h1>
